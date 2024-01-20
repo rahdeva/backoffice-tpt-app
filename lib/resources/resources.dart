b5327ca5fcc0d5b99fb2db64f66db8ac1daddc9b
NgurahDeva <wirandanaputra@gmail.com>
2024-01-21 00:14:23 +0800

    [UI] login

--- lib/resources/resources.dart
+++ lib/resources/resources.dart
@@ -6,25 +6,72 @@ import '/resources/assets.gen.dart'
 final AppImages = asset.Assets.lib.resources.images;
 
 class AppColors {
+  static const appBar = Color(0xFFFFFFFF);
+  static const colorPrimary = Color(0xFF075985);
+  static const colorSecondary = Color(0xFFA8AAAE);
+  static const success = Color(0xFF28C76F);
+  static const danger = Color(0xFFEA5455);
+  static const warning = Color(0xFFFF9F43);
+  static const info = Color(0xFF00CFE8);
+  static const light = Color(0xFFDFDFE3);
+  static const dark = Color(0xFF4B4B4B);
+  static const backgroundWhite = Color(0xFFF8F8F8);
+  static const surface = Color(0xFFFFFFFF);
+
+  // design system
   static const white = Color(0xFFFFFFFF);
-  static const black = Color(0xFF000000);
-  static const blue = Color(0xFF007AFF);
-  static const red = Color(0xFFFF3B30);
-  static const iconText = Color(0xFF354052);
-  static const iconBG = Color(0xFFECF6FD);
-  static const purple500 = Color(0xFF805AD5);
-  static const blue500 = Color(0xFF3182CE);
-  static const yellow500 = Color(0xFFD69E2E);
-  static const red500 = Color(0xFFE53E3E);
-  static const red50 = Color(0xFFFFF5F5);
-  static const green500 = Color(0xFF38A169);
-  static const gray100 = Color(0xFFEDF2F7);
-  static const gray200 = Color(0xFFE2E8F0);
-  static const gray300 = Color(0xFFCBD5E0);
-  static const gray500 = Color(0xFF718096);
-  static const purpleGradient = Color(0xFF9747FF);
-  static const blueGradient = Color(0xFF007AFF);
-  static const yellowGradient = Color(0xFFF4CE14);
-  static const redGradient = Color(0xFFFF4B91);
-  static const greenGradient = Color(0xFF01DBC2);
+  static const black = Colors.black;
+  static const grey = Colors.grey;
+  static const background1 = Color(0xFF212332);
+  static const background2 = Color(0xFF2A2D3E);
+  static const primary = Color(0xFF6D28D9);
+  static const pink = Color(0xFFF0ABFC);
+  static const yellow = Color(0xFFFCD34D);
+  static const red = Color(0xFFF87171);
+  static const blue = Color(0xFF7DD3FC);
+  static const green = Color(0xFF86EFAC);
+  static const orange = Color(0xFFFB923C);
+  static const greenGradient1 = Color(0xFF08774C);
+  static const greenGradient2 = Color(0xFFDEFD88);
+  static const blueGradient1 = Color(0xFF082777);
+  static const blueGradient2 = Color(0xFF88A9FD);
+  static const cyanGradient1 = Color(0xFF086478);
+  static const cyanGradient2 = Color(0xFF88E7FD);
+  static const redGradient1 = Color(0xFF780831);
+  static const redGradient2 = Color(0xFFFD88B3);
+  static const chocoGradient1 = Color(0xFF782308);
+  static const chocoGradient2 = Color(0xFFFDA388);
+  static const purpleGradient1 = Color(0xFF250878);
+  static const purpleGradient2 = Color(0xFFA788FD);
+
+  // textColour
+  static const textColour00 = Color(0xFFFFFFFF);
+  static const textColour10 = Color(0xFFE7E7E7);
+  static const textColour20 = Color(0xFFCFCFCF);
+  static const textColour30 = Color(0xFFB6B6B6);
+  static const textColour40 = Color(0xFF9E9E9E);
+  static const textColour50 = Color(0xFF868686);
+  static const textColour60 = Color(0xFF6E6E6E);
+  static const textColour70 = Color(0xFF565656);
+  static const textColour80 = Color(0xFF3D3D3D);
+  static const textColour90 = Color(0xFF252525);
+  static const textColour100 = Color(0xFF0D0D0D);
+
+  // gray
+  static const gray25 = Color.fromRGBO(75, 70, 92, 0.015);
+  static const gray50 = Color.fromRGBO(75, 70, 92, 0.030);
+  static const gray100 = Color.fromRGBO(75, 70, 92, 0.1);
+  static const gray200 = Color.fromRGBO(75, 70, 92, 0.2);
+  static const gray300 = Color.fromRGBO(75, 70, 92, 0.3);
+  static const gray400 = Color.fromRGBO(75, 70, 92, 0.4);
+  static const gray500 = Color.fromRGBO(75, 70, 92, 0.5);
+  static const gray600 = Color.fromRGBO(75, 70, 92, 0.6);
+  static const gray700 = Color.fromRGBO(75, 70, 92, 0.7);
+  static const gray800 = Color.fromRGBO(75, 70, 92, 0.8);
+  static const gray900 = Color.fromRGBO(75, 70, 92, 0.9);
+
+  // oldest
+  static const colorAccent = Colors.white;
+  static const borderColor = Colors.black12;
+  static const subHintColor = Colors.black45;
 }
